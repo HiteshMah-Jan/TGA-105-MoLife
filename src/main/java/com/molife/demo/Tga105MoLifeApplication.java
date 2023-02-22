679a4f0a2d5a805a6e688dc80be96e2c2369871a
Ryan <tototwgod5@gmail.com>
2023-02-22 23:58:54 +0800

    02/22 專題git創建

---
+++ src/main/java/com/molife/demo/Tga105MoLifeApplication.java
@@ -0,0 +1,13 @@
+package com.molife.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tga105MoLifeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tga105MoLifeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/molife/demo/Tga105MoLifeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.molife.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tga105MoLifeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
